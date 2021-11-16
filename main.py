06819e726b7a0323175b95c13875e77be34ecd0a
Jan Piaskowy <piaskowyjasiek@gmail.com>
2021-11-16 19:34:33 +0100

    Download data from api

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+# Spotify user nickname equals user id
+USER_ID = "wonderpol"
+# Spotify token generated in api
+TOKEN = "BQDjJO7G-YHoF9EIk6RmnExfkW_JCcxZ5MFY9kx3mkcNlEqoOHBFklEJOgCprgbIS6HTPItQUW8ggux3wc8wO94qpK1AI5QcuZad6A23k_K0M-SLRd3ZYwzbTOL5zd0NOsZFsgPcXBdDqGd-sAU"
 
+if __name__ == "__main__":
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {token} ".format(token=TOKEN)
+    }
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
+    req = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers=headers)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    data = req.json()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(data)
